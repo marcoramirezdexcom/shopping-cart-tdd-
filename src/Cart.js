6db4b520d14b8f37716df6a6ec2b075cb639980b
Marco Ramirez <marco.ramirez@dexcom.com>
2024-06-28 12:00:04 -0600

    Finished UT cases and development of the workflow

--- src/Cart.js
+++ src/Cart.js
@@ -1,5 +1,34 @@
 class Cart {
-  constructor() {}
+  constructor() {
+    this.items = []
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  getTotalNumberOfItems() {
+    return this.items.length;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItemByID(id) {
+    var removeIndex = this.items.map(item => item.id).indexOf(id);
+    this.items.splice(removeIndex, 1);
+  }
+
+  getTotalPriceOfItems() {
+    var total = 0;
+
+    this.items.forEach((item) => {
+      total += item.price;
+    });
+
+    return total;
+  }
 }
 
 module.exports = Cart;
--- src/Cart.test.js
+++ src/Cart.test.js
@@ -3,3 +3,82 @@
 // should add items to the shopping cart
 // should remove an item by itemId
 // should get the total price of the cart items
+
+const Chance = require("chance");
+const Cart = require("./Cart");
+
+const chance = new Chance();
+
+describe("when using Cart object", () => {
+    beforeEach(() => {
+        cart = new Cart();
+    });
+
+    it("should get the cart items", () => {
+        const item = {
+            id: chance.guid(),
+            price: chance.integer(),
+        }
+
+        cart.addItem(item);
+
+        const cartItems = cart.getItems();
+        expect(cartItems).toBeDefined();
+    });
+
+    it("should get the total number of cart items", () => {
+        const item = {
+            id: chance.guid(),
+            price: chance.integer(),
+        }
+
+        cart.addItem(item);
+
+        const cartItemsNumber = cart.getTotalNumberOfItems();
+        expect(cartItemsNumber).toBeDefined();
+    });
+
+    it("should add items to the shopping cart", () => {
+        const item = {
+            id: chance.guid(),
+            price: chance.integer(),
+        }
+
+        cart.addItem(item);
+
+        expect(cart.items).toContainEqual(item);
+    });
+
+    it("should remove an item by item ID", () => {
+        const item = {
+            id: chance.guid(),
+            price: chance.integer(),
+        }
+
+        cart.addItem(item);
+
+        let items = cart.getTotalNumberOfItems();
+
+        cart.removeItemByID(item.id);
+        expect(cart.getTotalNumberOfItems()).toEqual(items - 1);
+    });
+
+    it("should get the total price of the cart items", () => {
+        const item = {
+            id: chance.guid(),
+            price: chance.integer(),
+        }
+
+        const item2 = {
+            id: chance.guid(),
+            price: chance.integer(),
+        }
+
+        cart.addItem(item);
+        cart.addItem(item2);
+
+        let total = cart.getTotalPriceOfItems();
+
+        expect(cart.getTotalPriceOfItems()).toEqual(total)
+    });
+});
